31e1055711fb8530dd053846d02bade2549a3b5d
Ngoguey42 <ngoguey@student.42.fr>
2016-11-12 14:50:16 +0100

    Improve 33.cpp

--- 33.cpp
+++ 33.cpp
@@ -1,42 +1,11 @@
 
 #include <iostream>
-#include <array>
 #include <cmath>
 #include <algorithm>
 #include <iterator>
 #include <vector>
 #include <set>
-
-static int next_number(int i) {
-  i++;
-  if (i % 10 == 0)
-    return i + 1;
-  else
-    return i;
-}
-
-static bool double_same(double a, double b) {
-  return std::fabs(a - b) < 0.0001;
-}
-
-static double divide(int n, int d) {
-  return  static_cast<double>(n) / static_cast<double>(d);
-}
-
-static bool is_curious(int n, int d) {
-  const double fract = divide(n, d);
-
-  if (n % 10 == d % 10 && double_same(fract, divide(n / 10, d / 10)))
-    return true;
-  else if (n / 10 == d / 10 && double_same(fract, divide(n % 10, d % 10)))
-    return true;
-  else if (n % 10 == d / 10 && double_same(fract, divide(n / 10, d % 10)))
-    return true;
-  else if (n / 10 == d % 10 && double_same(fract, divide(n % 10, d / 10)))
-    return true;
-  else
-    return false;
-}
+#include <deque>
 
 static std::set<int> factors(int nbr) {
   std::set<int> factors;
@@ -73,31 +42,67 @@ static std::pair<int, int> simplify_fraction(int n, int d) {
   return {n, d};
 }
 
+class CuriousFractions {
+
+  int _n, _d;
+
+  int _next_number(int i) {
+    i++;
+    if (i % 10 == 0)
+      return i + 1;
+    else
+      return i;
+  }
+
+  bool _double_same(double a, double b) {
+    return std::fabs(a - b) < 0.0001;
+  }
+
+  double _divide(int n, int d) {
+    return  static_cast<double>(n) / static_cast<double>(d);
+  }
+
+  bool _is_curious() {
+    const double fract = _divide(_n, _d);
+
+    if (_n % 10 == _d % 10 && _double_same(fract, _divide(_n / 10, _d / 10)))
+      return true;
+    else if (_n / 10 == _d / 10 && _double_same(fract, _divide(_n % 10, _d % 10)))
+      return true;
+    else if (_n % 10 == _d / 10 && _double_same(fract, _divide(_n / 10, _d % 10)))
+      return true;
+    else if (_n / 10 == _d % 10 && _double_same(fract, _divide(_n % 10, _d / 10)))
+      return true;
+    else
+      return false;
+  }
+
+public:
+  std::deque<std::pair<int, int>> operator()() {
+    std::deque<std::pair<int, int>> curious_fractions;
+
+    for (_n = 11; _n <= 98; _n = _next_number(_n))
+      for (_d = _next_number(_n); _d <= 99; _d = _next_number(_d))
+        if (_is_curious())
+          curious_fractions.emplace_back(_n, _d);
+    return curious_fractions;
+  }
+
+};
+
 int main() {
   int curious_n, curious_d;
 
   curious_n = 1;
   curious_d = 1;
-  for (int n = 11; n <= 98; n = next_number(n)) {
-    for (int d = next_number(n); d <= 99; d = next_number(d)) {
-      if (is_curious(n, d)) {
-        std::cout
-          << "n(" << n << ") / "
-          << "d(" << d << ") "
-          << std::endl;
-        curious_n *= n;
-        curious_d *= d;
-      }
-    }
+  for (auto pair: CuriousFractions{}()) {
+    std::cout << pair.first << "/" << pair.second << std::endl;
+    curious_n *= pair.first;
+    curious_d *= pair.second;
   }
-  std::cout
-    << "curious_n(" << curious_n << ") / "
-    << "curious_d(" << curious_d << ")"
-    << std::endl;
+
+  std::cout << curious_n << "/" << curious_d << std::endl;
   std::tie(curious_n, curious_d) = simplify_fraction(curious_n, curious_d);
-  std::cout
-    << "curious_n(" << curious_n << ") / "
-    << "curious_d(" << curious_d << ")"
-    << std::endl;
+  std::cout << curious_n << "/" << curious_d << std::endl;
   return 0;
 }
